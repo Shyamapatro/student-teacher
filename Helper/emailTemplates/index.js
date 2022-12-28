bf51ddca8215e3e4fd42ac35d43a57a5729c3393
shyamapatro <shyama.patro@gmail.com>
2022-12-28 18:13:51 +0530

    code init

--- Helper/emailTemplates/index.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-	forgotPassword: require("./forgotPassword").forgotPassword,
-	registerUser: require("./registerUser").registerUser,
-	registerAdmin: require("./registerAdmin").registerAdmin,
-	forgotPasswordAdmin: require("./forgotPasswordAdmin").forgotPasswordAdmin
-};
